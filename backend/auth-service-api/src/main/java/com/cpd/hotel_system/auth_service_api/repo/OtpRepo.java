ddf8172c04e6ecf75aee477707d31e27566fd246
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-08-07 15:25:14 +0530

    Create repositories for the Otp and SystemUser

---
+++ backend/auth-service-api/src/main/java/com/cpd/hotel_system/auth_service_api/repo/OtpRepo.java
@@ -0,0 +1,6 @@
+package com.cpd.hotel_system.auth_service_api.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OtpRepo extends JpaRepository<OtpRepo,String> {
+}
