fe4bd8114004a8eb7c59fe87aaa161f7ba869115
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-08-07 16:49:11 +0530

    Apply the createuser method    - dto validation

--- backend/auth-service-api/src/main/java/com/cpd/hotel_system/auth_service_api/repo/OtpRepo.java
+++ backend/auth-service-api/src/main/java/com/cpd/hotel_system/auth_service_api/repo/OtpRepo.java
@@ -1,6 +1,12 @@
 package com.cpd.hotel_system.auth_service_api.repo;
 
+import com.cpd.hotel_system.auth_service_api.entity.Otp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OtpRepo extends JpaRepository<OtpRepo,String> {
+import java.util.Optional;
+
+public interface OtpRepo extends JpaRepository<OtpRepo, String> {
+    @Query(value = "SELECT  * FROM Otp WHERE system_user_id=?1",nativeQuery=true)
+    public Optional<Otp> findBySystemUserId(String id);
 }
--- backend/auth-service-api/src/main/java/com/cpd/hotel_system/auth_service_api/service/impl/SystemUserServiceImpl.java
+++ backend/auth-service-api/src/main/java/com/cpd/hotel_system/auth_service_api/service/impl/SystemUserServiceImpl.java
@@ -1,18 +1,37 @@
 package com.cpd.hotel_system.auth_service_api.service.impl;
 
+import com.cpd.hotel_system.auth_service_api.config.KeyClockSecurityUtil;
 import com.cpd.hotel_system.auth_service_api.dto.request.SystemUserRequestDto;
+import com.cpd.hotel_system.auth_service_api.entity.Otp;
+import com.cpd.hotel_system.auth_service_api.entity.SystemUser;
 import com.cpd.hotel_system.auth_service_api.exceptions.BadRequestException;
+import com.cpd.hotel_system.auth_service_api.exceptions.DuplicateEntryException;
+import com.cpd.hotel_system.auth_service_api.repo.OtpRepo;
 import com.cpd.hotel_system.auth_service_api.repo.SystemUserRepo;
 import com.cpd.hotel_system.auth_service_api.service.SystemUserService;
 
 import lombok.RequiredArgsConstructor;
 import org.keycloak.admin.client.Keycloak;
+//import org.keycloak.representations.account.UserRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SystemUserServiceImpl implements SystemUserService {
+
+    @Value("${keycloak.config.realm}")
+    private String realm;
+
     private final SystemUserRepo systemUserRepo;
+    private final KeyClockSecurityUtil keyClockUtil;
+    private final OtpRepo otpRepo;
 
     @Override
     public void createUser(SystemUserRequestDto dto) {
@@ -31,5 +50,35 @@ public class SystemUserServiceImpl implements SystemUserService {
         String otp = "";
         Keycloak keycloak = null;
 
+        UserRepresentation existingUser = null;
+        keycloak = keyClockUtil.getKeyClockInstance();
+
+        keycloak.realm(realm).users().search(dto.getEmail()).stream().findFirst().orElse(null);
+
+        if (existingUser != null) {
+            Optional<SystemUser> selectedSystemUserFromAuthService =
+                    systemUserRepo.findByEmail(dto.getEmail());
+            if (selectedSystemUserFromAuthService.isEmpty()) {
+                keycloak.realm(realm).users().delete(existingUser.getId());
+            } else {
+                throw new DuplicateEntryException("Email already exists");
+            }
+        } else {
+            Optional<SystemUser> selectedSystemUserFromAuthService =
+                    systemUserRepo.findByEmail(dto.getEmail());
+            if (selectedSystemUserFromAuthService.isPresent()) {
+                Optional<Otp> selectedOtp =
+                        otpRepo.findBySystemUserId(selectedSystemUserFromAuthService.get().getUserId());
+                if (selectedOtp.isPresent()) {
+                    otpRepo.deleteById(selectedOtp.get().getPropertyId());
+                }
+                systemUserRepo.deleteById(selectedSystemUserFromAuthService.get().getUserId());
+            }
+        }
+        //TODO need to complete
+
     }
+
+   
+
 }
